05b33c6dc67d10a76e736a51e6afb7d4aa3d41ec
Tyler McKell <tmckell@devpipeline.com>
2024-01-22 18:50:41 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import "./styles/app.scss";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="page-container">
+        <div className="title">
+          This is the Rainbow Box
+          <p>
+            (to help strengthen your use of flex-direction, justify-content, &
+            align-items)
+          </p>
+        </div>
+        <div className="margin">
+          <div className="border">
+            <div className="padding">
+              <div className="content"></div>
+              <div className="vertical-line">
+                {" "}
+                {`<--cross axis (align-items)`}
+              </div>
+              <div className="horizontal-line">{`^--main axis (justify-content)`}</div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
